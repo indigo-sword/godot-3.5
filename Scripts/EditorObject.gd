cefefe10f595c52f33201d51309393e64135ad2d
Meng Chen <mchen24@nd.edu>
2024-03-04 11:34:07 -0500

    add zoom-in

--- Scripts/EditorObject.gd
+++ Scripts/EditorObject.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 var can_place = true
+var is_planning = true
 var current_item
+export var cam_spd = 10
+
 onready var level = get_node("/root/LevelEditor/Level")
+onready var editor = get_node("/root/LevelEditor/CamContainer")
+onready var editor_cam = editor.get_node("Camera2D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	editor_cam.current = true
 	pass # Replace with function body.
 
 
@@ -17,4 +23,34 @@ func _process(delta):
 		var new_item = current_item.instance()
 		level.add_child(new_item)
 		new_item.global_position = global_position
+	
+	move_editor()
+	is_planning = Input.is_action_pressed("mb_middle")
 	pass
+	
+func move_editor():
+	if Input.is_action_pressed("w"):
+		editor.global_position.y -= cam_spd
+	if Input.is_action_pressed("a"):
+		editor.global_position.x -= cam_spd
+	if Input.is_action_pressed("s"):
+		editor.global_position.y += cam_spd
+	if Input.is_action_pressed("d"):
+		editor.global_position.x += cam_spd
+	pass
+	
+func _unhandled_input(event):
+	if (event.is_action_pressed("zoom_in")):
+		editor_cam.zoom -= Vector2(0.1, 0.1)
+	if (event.is_action_pressed("zoom_out")):
+		editor_cam.zoom += Vector2(0.1, 0.1)
+	if (event is InputEventMouseButton):
+		if (event.is_pressed()):
+			if (event.button_index == BUTTON_WHEEL_UP):
+				editor_cam.zoom -= Vector2(0.1, 0.1)
+			if (event.button_index == BUTTON_WHEEL_DOWN):
+				editor_cam.zoom += Vector2(0.1, 0.1)	
+	if (event is InputEventMouseMotion):
+		if (is_planning):
+			editor.global_position -= event.relative + editor_cam.zoom
+
--- Scripts/ScrollContainer.gd
+++ Scripts/ScrollContainer.gd
@@ -1,26 +1,25 @@
-extends TabContainer
+extends ScrollContainer
 
 onready var object_cursor = get_node("/root/LevelEditor/EditorObject")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("mouse_entered", self, "mouse_enter")
+	connect("mouse_exited", self, "mouse_leave")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func _on_TabContainer_mouse_entered():
+
+func mouse_enter():
 	object_cursor.can_place = false
 	object_cursor.hide()
 	pass
 	
-
-func _on_TabContainer_mouse_exited():
+func mouse_leave():
 	object_cursor.can_place = true
 	object_cursor.show()
 	pass
