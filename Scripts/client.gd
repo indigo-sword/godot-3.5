5a8c76cfbc55f72a5ada325af4cd7c6bb99a11af
joao <Joao.Henares@btgpactual.com>
2024-03-08 12:39:03 -0300

    creating client object and script

--- Scripts/client.gd
+++ Scripts/client.gd
@@ -1,48 +1,43 @@
 extends Control
 
+#### IMPORTANT: we should use a variable to detect anywhere 
+#### in the game whether the user is logged in.
+#### how do we do that? i'm using globals for the moment
+var LOGGED_IN = false
+var COOKIE = ""
 var URL = "http://64.225.11.30:8080"
 
-var cookie = ""
-var username = "LsZCFWRNMl"
-var password = "PASS"
+# for testing purposes
+var SAMPLE_UNAME = "LsZCFWRNMl"
+var SAMPLE_PASS = "PASS"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	pass 
+	
 func login(username: String, password: String):
 	var headers = ["Content-Type: application/x-www-form-urlencoded"]
 	var body = "username=" + username + "&password=" + password
 	
-	$login.request(URL + "/login", headers, HTTPClient.METHOD_POST, body)
+	return $LoginRequest.request(URL + "/login", headers, HTTPClient.METHOD_POST, body)
 
-
-func _on_login_request_completed(result, response_code, headers, body):
+func _on_LoginRequest_request_completed(result, response_code, headers, body):
 	var output = JSON.parse_string(body.get_string_from_utf8())
 	if response_code == 200:
 		for header in headers:
 			if "Set-Cookie" in header:
-				cookie = header.split(" ")[1].split(";")[0]
-				print(cookie)
-
-func _on_login_button_pressed():
-	login(username, password)
-
+				COOKIE = header.split(" ")[1].split(";")[0]
+				print(COOKIE)
+	
+	return COOKIE 
+	
 func change_bio(username: String, new_bio: String):
-	var headers = ["Content-Type: application/x-www-form-urlencoded", "Cookie: " + cookie]
+	var headers = ["Content-Type: application/x-www-form-urlencoded", "Cookie: " + COOKIE]
 	var body = "username=" + username + "&bio=" + new_bio
 	
-	$changeBio.request(URL + "/change_user_bio", headers, HTTPClient.METHOD_POST, body)
-	
-func _on_change_bio_button_pressed():
-	change_bio(username, "some new bio")
+	return $ChangeBioRequest.request(URL + "/change_user_bio", headers, HTTPClient.METHOD_POST, body)
 
-func _on_change_bio_request_completed(result, response_code, headers, body):
+func _on_ChangeBioRequest_request_completed(result, response_code, headers, body):
 	var output = JSON.parse_string(body.get_string_from_utf8())
 	var message = output["message"]
 	var bio = output["bio"]
