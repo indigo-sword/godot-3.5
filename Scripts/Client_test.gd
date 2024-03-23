e9d85c740cfee899b162f0f27fa767837e2192df
joao <Joao.Henares@btgpactual.com>
2024-03-23 02:50:47 -0400

    creating client functions, up to change user bio

--- Scripts/Client_test.gd
+++ Scripts/Client_test.gd
@@ -1,54 +1,140 @@
 extends Node2D
 
+func RANDOM_UNAME():
+	var l = 60
+	var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+	var uname = ""
+	for i in range(l):
+		uname += characters[rand_range(0, characters.length())]
+	return uname
+
+func RANDOM_PASS():
+	var l = 25
+	var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_!@#$%^*()-+=[]{}|;:,.<>?`~"
+	var password = ""
+	for i in range(l):
+		password += characters[rand_range(0, characters.length())]
+	return password
+
+func RANDOM_EMAIL():
+	var uname = RANDOM_UNAME()
+	return uname + "@gmail.com"
+	
 func _on_login_pressed():
 	var SAMPLE_UNAME = "LsZCFWRNMl"
 	var SAMPLE_PASS = "PASS"
+	var err = ""
 	
-	# test login
-	print("LOGIN:")
-	Client.login(SAMPLE_UNAME, SAMPLE_PASS)
-	if not Client.ERROR: yield(Client, "login_completed")
+	#### test login -- SUCCESS
+	print("test login -- SUCCESS") 
+	err = Client.login(SAMPLE_UNAME, SAMPLE_PASS)
+	if err == "": yield(Client, "login_completed")
+	else: 
+		print("TESTING ERROR: got error at function: ", err)
+		return
 	
-	if not Client.LOGGED_IN or Client.ERROR != "": print(Client.ERROR)
-	else: print("ok")
+	if Client.ERROR != "": 
+		print("TESTING ERROR: got error at client: ", Client.ERROR)
+		return
 	
-	# test login fail
-	print("LOGIN ALREADY LOGGED IN:")
-	Client.login(SAMPLE_UNAME, SAMPLE_PASS)
-	if not Client.ERROR: yield(Client, "login_completed")
-	if Client.ERROR != "already logged in": print(Client.ERROR)
-	else: print("ok")
+	print("ok")
 	
-	# test logout
-	print("LOGOUT:")
-	Client.logout()
-	if not Client.ERROR: yield(Client, "logout_completed")
+	#### test login -- FAIL: already logged in
+	print("test login -- FAIL: already logged in")
+	err = Client.login(SAMPLE_UNAME, SAMPLE_PASS)
+	if err != "Already logged in": 
+		print("TESTING ERROR: got wrong error: ", err)
+		return
+
+	if Client.ERROR != "": 
+		print("TESTING ERROR: got error at client: ", Client.ERROR)
+		return
 	
-	if Client.LOGGED_IN or Client.ERROR != "": print(Client.ERROR)
-	else: print("ok")
+	print("ok")
 	
-	# test logout fail
-	print("LOGOUT ALREADY LOGGED OUT:")
-	Client.logout()
-	if not Client.ERROR: yield(Client, "logout_completed")
-	if Client.ERROR != "not logged in": print(Client.ERROR)
-	else: print("ok")
+	#### test login -- FAIL: wrong password
+	# this was just testing whether we got the right 
+	# error messages
+	print("test login -- FAIL: wrong password")
 	
-	Client.login(SAMPLE_UNAME, SAMPLE_PASS)
-	if not Client.ERROR: yield(Client, "login_completed")
+	err = Client.logout()
+	yield(Client, "logout_completed")
+	if Client.LOGGED_IN: 
+		print("TESTING ERROR - logout function")
+		return
+		
+	err = Client.login(SAMPLE_UNAME, "wrong password")
+	if err == "": yield(Client, "login_completed")
 	
-	# test change bio
-	print("CHANGE BIO:")
+	if Client.ERROR != "invalid password":
+		print("TESTING ERROR: got wrong error at client: ", Client.ERROR)
+		return
+		
+	if Client.ERROR_CODE != 401:
+		print("TESTING ERROR: got wrong error code at client: ", Client.ERROR_CODE)
+		
+	print("ok")
 	
-	Client.change_bio("hello this is my new bio")
-	if not Client.ERROR: yield(Client, "change_bio_completed")
+	#### test logout -- SUCCESS
+	err = Client.login(SAMPLE_UNAME, SAMPLE_PASS)
+	if err == "": yield(Client, "login_completed")
 	
-	if Client.ERROR != "": print(Client.ERROR)
-	else: print("ok")
+	print("test logout -- SUCCESS")
+	err = Client.logout()
+	if err == "": yield(Client, "logout_completed")
 	
-	# test change bio fail
-	print("CHANGE BIO WITH &:")
-	Client.change_bio("toin & jonas")
+	if Client.ERROR != "": 
+		print("TESTING ERROR: got error at client: ", Client.ERROR)
+		return
+		
+	print("ok")
 	
-	if Client.ERROR != "invalid character: &": print(Client.ERROR)
-	else: print("ok")
+	#### test logout --  FAIL: already logged out
+	print("test logout --  FAIL: already logged out")
+	err = Client.logout()
+	if err != "not logged in":
+		print("TESTING ERROR: no error when should have")
+		return
+		
+	print("ok")
+	
+	err = Client.login(SAMPLE_UNAME, SAMPLE_PASS)
+	if err == "": yield(Client, "login_completed")
+	
+	#### test create user -- SUCCESS
+	print("test create user -- SUCCESS")
+	var t = Client.create_user(RANDOM_UNAME(), RANDOM_PASS(), RANDOM_EMAIL())
+	if t == "": yield(Client, "create_user_completed")
+	else:
+		print("TESTING ERROR: got error when should not have")
+		return
+	
+	if Client.ERROR_CODE != 0:
+		print("TESTING ERROR: got error in client when should not have")
+		print(Client.ERROR)
+		return
+	
+	print("ok")
+		
+	#### test change bio -- SUCCESS
+	print("test change bio -- SUCCESS")
+	
+	err = Client.change_bio("hello this is my new bio")
+	if err == "": yield(Client, "change_bio_completed")
+	else: 
+		print("TESTING ERROR: got error ", err)
+		return
+	
+	if Client.ERROR != "": 
+		print("TESTING ERROR: got error when should not have ", Client.ERROR)
+		return
+		
+	print("ok")
+
+	#### test change bio -- FAIL: has &
+	print("test change bio -- FAIL: has &")
+	err = Client.change_bio("toin & jonas")
+	if err != "invalid character: &": 
+		print("TESTING ERROR: got no error when should have")
+		
+	print("ok")
--- Scripts/client.gd
+++ Scripts/client.gd
@@ -1,17 +1,21 @@
 extends Control
 
+const GODOT_CLIENT_ERRORS = preload("res://Scripts/client_errors.gd")
+
 # class attributes
 var LOGGED_IN: bool
 var COOKIE: String
 var URL: String
 var UNAME: String
 var ERROR: String
+var ERROR_CODE: int
 var BIO: String
 
 # signals
 signal login_completed
 signal logout_completed
 signal change_bio_completed
+signal create_user_completed
 
 func _init(): 
 	self.LOGGED_IN = false
@@ -19,42 +23,58 @@ func _init():
 	self.URL = "http://64.225.11.30:8080"
 	self.UNAME = ""
 	self.ERROR = ""
+	self.ERROR_CODE = 0
 	self.BIO = ""
 	
 func login(username: String, password: String):
 	if self.LOGGED_IN: 
-		self.ERROR = "already logged in"
-		return
-	self.ERROR = ""
+		return "Already logged in"
 	
 	self.UNAME = username
 	
 	var headers = ["Content-Type: application/x-www-form-urlencoded"]
 	var body = "username=" + username + "&password=" + password
-	
-	$LoginRequest.request(self.URL + "/login", headers, true, HTTPClient.METHOD_POST, body)
 
+	$LoginRequest.request(self.URL + "/login", headers, true, HTTPClient.METHOD_POST, body)
+	return ""
+	
 func _on_LoginRequest_request_completed(result, response_code, headers, body):
+	var output = parse_json(body.get_string_from_utf8())
+	var got_cookie = false
+	
 	if response_code == 200:
 		for header in headers:
 			if "Set-Cookie" in header:
 				self.COOKIE = header.split(" ")[1].split(";")[0]
 				self.LOGGED_IN = true
 				self.ERROR = ""
-	
-	else: self.ERROR = response_code
+				self.ERROR_CODE = 0
+				got_cookie = true
+		
+		if not got_cookie:
+			self.ERROR = "server error - no cookie (this should not happen)"
+
+	else: 
+		if result != 0: 
+			self.ERROR = GODOT_CLIENT_ERRORS.E.get(result, "unknown error")
+			self.ERROR_CODE = result
+		else: 
+			self.ERROR = output.get("message", "unknown server error")
+			self.ERROR_CODE = response_code
+		
 	emit_signal("login_completed")
+	
 	
 func logout():
 	if not self.LOGGED_IN:
-		self.ERROR = "not logged in"
-		return
-	self.ERROR = ""
+		return "not logged in"
 		
 	var headers = ["Content-Type: application/x-www-form-urlencoded", "Cookie: " + COOKIE]
 	var body = "username=" + self.UNAME 
 	
 	$LogoutRequest.request(self.URL + "/logout", headers, true, HTTPClient.METHOD_POST, body)
+	
+	return ""
 	
 func _on_LogoutRequest_request_completed(result, response_code, headers, body):
 	var output = parse_json(body.get_string_from_utf8())
@@ -62,24 +82,56 @@ func _on_LogoutRequest_request_completed(result, response_code, headers, body):
 	if response_code == 200:
 		self.LOGGED_IN = false
 		self.ERROR = ""
+		self.ERROR_CODE = 0
 		
-	else: self.ERROR = response_code
+	else: 
+		if result != 0: 
+			self.ERROR = GODOT_CLIENT_ERRORS.E.get(result, "unknown error")
+			self.ERROR_CODE = result
+		else: 
+			self.ERROR = output.get("message", "unknown server error")
+			self.ERROR_CODE = response_code
+			
 	emit_signal("logout_completed")
+	
+	
+func create_user(username: String, password: String, email: String):	
+	if "&" in username or "&" in password or "&" in email:
+		return "invalid character: &"
+	
+	var headers = ["Content-Type: application/x-www-form-urlencoded"]
+	var body = "username=" + username + "&password=" + password + "&email=" + email	
+	$CreateUserRequest.request(self.URL + "/create_user", headers, true, HTTPClient.METHOD_POST, body)
+	return ""
+	
+func _on_CreateUserRequest_request_completed(result, response_code, headers, body):
+	var output = parse_json(body.get_string_from_utf8())
+	if response_code == 201:
+		self.ERROR = ""
+		self.ERROR_CODE = 0	
+		
+	else:
+		if result != 0: 
+			self.ERROR = GODOT_CLIENT_ERRORS.E.get(result, "unknown error")
+			self.ERROR_CODE = result
+		else:
+			self.ERROR = output.get("message", "unknown server error")
+			self.ERROR_CODE = response_code
+	
+	emit_signal("create_user_completed")
 
 func change_bio(new_bio: String):
 	if not(self.LOGGED_IN):
-		self.ERROR = "not logged in"
-		return
+		return "not logged in"
 		
 	if "&" in new_bio: 
-		self.ERROR = "invalid character: &"
-		return
-	self.ERROR = ""
+		return "invalid character: &"
 	
 	var headers = ["Content-Type: application/x-www-form-urlencoded", "Cookie: " + COOKIE]
 	var body = "username=" + self.UNAME + "&bio=" + new_bio
 	
 	$ChangeBioRequest.request(URL + "/change_user_bio", headers, true, HTTPClient.METHOD_POST, body)
+	return ""
 	
 func _on_ChangeBioRequest_request_completed(result, response_code, headers, body):
 	var output = parse_json(body.get_string_from_utf8())
@@ -87,6 +139,14 @@ func _on_ChangeBioRequest_request_completed(result, response_code, headers, body
 	if response_code == 200:
 		self.BIO = output["bio"]
 		self.ERROR = ""
+		self.ERROR_CODE = 0
 		
-	else: self.ERROR = response_code
+	else: 
+		if result != 0: 
+			self.ERROR = GODOT_CLIENT_ERRORS.E.get(result, "unknown error")
+			self.ERROR_CODE = result
+		else: 
+			self.ERROR = output.get("message", "unknown server error")
+			self.ERROR_CODE = response_code
+			
 	emit_signal("change_bio_completed")
