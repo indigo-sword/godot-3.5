eff8cf41b830f7ebba0367c2e901909e966dcd5f
Meng Chen <mchen24@nd.edu>
2024-03-30 15:16:29 -0400

    organize level editor

--- Scripts/TabContainer.gd
+++ Scripts/TabContainer.gd
@@ -1,9 +1,6 @@
 extends TabContainer
 
 onready var object_cursor = get_node("/root/LevelEditor/EditorObject")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
