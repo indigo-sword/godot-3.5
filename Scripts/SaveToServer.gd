72cb89e62c9d9daada93739f50a8cf9b4b78b4bf
Meng Chen <mchen24@nd.edu>
2024-04-01 21:43:48 -0400

    update new ui

--- Scripts/SaveToServer.gd
+++ Scripts/SaveToServer.gd
@@ -1,14 +1,24 @@
 extends Button
 
+onready var level = get_node("/root/LevelEditor/Level")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect the button's 'pressed' signal to the 'on_button_pressed' method.
 	connect("pressed", self, "on_button_pressed")
-
-# This method will be called when the button is pressed
+	
 func on_button_pressed():
-	# Call the 'serialize_scene' method with the path of the scene to serialize and output file path
-	serialize_scene("res://path_to_your_scene.tscn", "res://output_file.dat")
+	# Assuming 'level' is a variable pointing to the node you want to pack
+	save_packed_scene(level)
+	serialize_scene("res://saved_scene.tscn", "res://output_file.dat")
+
+func save_packed_scene(level_node):
+	var toSave : PackedScene = PackedScene.new()
+	if toSave.pack(level_node) == OK:
+		var scene_path = "res://saved_scene.tscn" # Or any other path you want
+		ResourceSaver.save(scene_path, toSave)
+		print("Packed scene saved successfully to", scene_path)
+	else:
+		print("Failed to pack the scene.")
 
 # Serializes the current scene with all its dependencies into a single file
 func serialize_scene(scene_path: String, output_file: String):
@@ -18,7 +28,7 @@ func serialize_scene(scene_path: String, output_file: String):
 		"scene": scene,
 		"dependencies": []
 	}
-
+	
 	for dep in deps:
 		var res = ResourceLoader.load(dep)
 		packed_data["dependencies"].append({"path": dep, "resource": res})
